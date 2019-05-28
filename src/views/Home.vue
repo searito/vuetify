f983484539ce58f306fb22c370761de7e6dd686f
Searito <searclimaco@gmail.com>
2019-05-27 22:57:17 -0600

    Breakpoints

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,38 +2,16 @@
   <div class="home">
     <h1>Inicio</h1>
 
-    <v-btn class="pink white--text">
-      Clickeame
+    <v-btn dark class="hidden-md-and-down" color="teal">
+      <v-icon>mouse</v-icon>
     </v-btn>
 
-    <v-btn dark color="teal darken-2">
-      Click a mi
+    <v-btn dark color="blue-gray" class="hidden-md-and-up">
+      <v-icon>how_to_vote</v-icon>
     </v-btn>
 
-    <v-btn dark drepressed color="light-blue darken-4">
-      Otro Click
-    </v-btn>
-
-    <v-btn flat color="amber">Más Click</v-btn>
-
-
-    <v-btn class="deep-orange white--text">
-      <v-icon left>translate</v-icon>
-      <span>Traducir</span>
-    </v-btn>
-
-    <v-btn class="white--text red" small>
-      <span>Touch Me</span>
-      <v-icon right small>fingerprint</v-icon>
-    </v-btn>
-
-    <v-btn class="white--text purple" large>
-      <span>Grande</span>
-      <v-icon right large>fullscreen</v-icon>
-    </v-btn>
-
-     <v-btn fab small dark color="red">
-      <v-icon>favorite</v-icon>
+    <v-btn dark color="deep-orange" class="hidden-sm-only">
+      <v-icon>queue_play_next</v-icon>
     </v-btn>
   </div>
 </template>
