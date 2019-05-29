b6b1c8459a2849c7801d3d316d48848791d44160
Searito <searclimaco@gmail.com>
2019-05-28 20:29:39 -0600

    Navigation Drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,9 @@
     <div>
         <nav>
             <v-toolbar flat app class="blue-grey darken-3">
+                <v-toolbar-side-icon class="grey--text" @click="drawer =! drawer">
+                </v-toolbar-side-icon>
+
                 <v-toolbar-title class="text-uppercase grey--text">
                     <span class="font-weight-light">Todo</span>
                     <span>Sear</span>
@@ -14,12 +17,20 @@
                     <v-icon right>power_settings_new</v-icon>
                 </v-btn>
             </v-toolbar>
+
+            <v-navigation-drawer v-model="drawer" app class="blue-grey lighten-1">
+                <p>Prueba</p>
+            </v-navigation-drawer>
         </nav>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            drawer: false,
+        }
+    },
 }
 </script>
