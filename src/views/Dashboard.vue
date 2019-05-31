ceaa98d8d463d2a2ba0859e50e65ee63203073c3
Searito <searclimaco@gmail.com>
2019-05-30 22:35:30 -0600

    Grilla Vuetify

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,19 +3,36 @@
     <h1 class="subheading blue-grey--text">Dashboard</h1>
 
     <v-container class="my-3">
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium sint illum perspiciatis! Nobis dolore, 
-        ullam ipsum corporis ab accusamus sapiente voluptatibus, nemo perferendis reprehenderit mollitia temporibus eius 
-        rerum, excepturi eum.
-      </p>
-      <p>
-        Labore porro quia quod, pariatur quae maiores nemo. Sunt qui natus doloremque nobis aspernatur quos dicta ex officia 
-        deleniti deserunt ut, vel corporis tempore quaerat odit alias sed fugit dignissimos.
-      </p>
-      <p>
-        Ex amet maxime deleniti odit tempore dicta sequi soluta saepe aliquam beatae neque molestias, quia culpa, corrupti 
-        libero! Id doloribus neque odio, sint harum molestias perferendis qui quia ipsa temporibus!
-      </p>
+      <v-card class="pa-3">
+        <v-layout row wrap>
+          <v-flex xs12 md6>
+            <div class="caption grey--text">
+              Proyecto
+            </div>
+            <div>Crear Nuevo Sitio</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">
+              Creador
+            </div>
+            <div>Searito's</div>
+          </v-flex>
+
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">
+              Fecha
+            </div>
+            <div>30/05/2019</div>
+          </v-flex>
+
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">
+              Status
+            </div>
+            <div>Diagramación</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
     </v-container>
   </div>
 </template>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,21 +3,44 @@
     <h1 class="subheading blue-grey--text">Equipo</h1>
 
      <v-container class="my-3">
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium sint illum perspiciatis! Nobis dolore, 
-        ullam ipsum corporis ab accusamus sapiente voluptatibus, nemo perferendis reprehenderit mollitia temporibus eius 
-        rerum, excepturi eum.
-      </p>
-      <p>
-        Labore porro quia quod, pariatur quae maiores nemo. Sunt qui natus doloremque nobis aspernatur quos dicta ex officia 
-        deleniti deserunt ut, vel corporis tempore quaerat odit alias sed fugit dignissimos.
-      </p>
-      <p>
-        Ex amet maxime deleniti odit tempore dicta sequi soluta saepe aliquam beatae neque molestias, quia culpa, corrupti 
-        libero! Id doloribus neque odio, sint harum molestias perferendis qui quia ipsa temporibus!
-      </p>
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-btn outline block class="primary">1</v-btn>
+        </v-flex>
+
+        <v-flex xs12 md6>
+          <v-btn outline block class="primary">2</v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap>
+        <v-flex xs3 md6>
+          <v-btn block outline color="success">3</v-btn>
+        </v-flex>
+
+        <v-flex xs3 md2>
+          <v-btn block outline color="success">4</v-btn>
+        </v-flex>
+
+        <v-flex xs3 md2>
+          <v-btn block outline color="success">5</v-btn>
+        </v-flex>
+
+        <v-flex xs3 md2>
+          <v-btn block outline color="success">6</v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap justify-center>
+        <v-flex xs4 md3>
+          <v-btn outline block class="warning">7</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="warning">8</v-btn>
+        </v-flex>
+      </v-layout>
     </v-container>
-      
+
   </div>
 </template>
 
