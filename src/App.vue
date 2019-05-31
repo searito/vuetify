014eb0e290e9fb0d559c8a8a8eb89252e742793f
Searito <searclimaco@gmail.com>
2019-05-30 20:22:54 -0600

    Padding & Margin

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app class="blue-grey lighten-5">
     <Navbar/>
-    <v-content>
+    <v-content class="mx-4 mb-4">
       <router-view></router-view>
     </v-content>
 
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="projects">
-    <h1>Proyectos</h1>    
+    <h1 class="subheading blue-grey--text">Proyectos</h1>
+
+     <v-container class="my-3">
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium sint illum perspiciatis! Nobis dolore, 
+        ullam ipsum corporis ab accusamus sapiente voluptatibus, nemo perferendis reprehenderit mollitia temporibus eius 
+        rerum, excepturi eum.
+      </p>
+      <p>
+        Labore porro quia quod, pariatur quae maiores nemo. Sunt qui natus doloremque nobis aspernatur quos dicta ex officia 
+        deleniti deserunt ut, vel corporis tempore quaerat odit alias sed fugit dignissimos.
+      </p>
+      <p>
+        Ex amet maxime deleniti odit tempore dicta sequi soluta saepe aliquam beatae neque molestias, quia culpa, corrupti 
+        libero! Id doloribus neque odio, sint harum molestias perferendis qui quia ipsa temporibus!
+      </p>
+    </v-container>
+       
   </div>
 </template>
 
