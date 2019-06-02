61e993c7819e0e6ff69521b4e1f263bf7159910a
Searito <searclimaco@gmail.com>
2019-06-02 14:27:31 -0600

    Cards en Sección Equipo

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,41 +2,24 @@
   <div class="team">
     <h1 class="subheading blue-grey--text">Equipo</h1>
 
-     <v-container class="my-3">
+    <v-container class="my-3">
       <v-layout row wrap>
-        <v-flex xs12 md6>
-          <v-btn outline block class="primary">1</v-btn>
-        </v-flex>
-
-        <v-flex xs12 md6>
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap>
-        <v-flex xs3 md6>
-          <v-btn block outline color="success">3</v-btn>
-        </v-flex>
-
-        <v-flex xs3 md2>
-          <v-btn block outline color="success">4</v-btn>
-        </v-flex>
-
-        <v-flex xs3 md2>
-          <v-btn block outline color="success">5</v-btn>
-        </v-flex>
-
-        <v-flex xs3 md2>
-          <v-btn block outline color="success">6</v-btn>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap justify-center>
-        <v-flex xs4 md3>
-          <v-btn outline block class="warning">7</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class="warning">8</v-btn>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card class="text-xs-center ma-3">
+            <v-responsive class="pt-4">
+              image goes here
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="grey lighten-5">
+                <v-icon small left>message</v-icon>
+                <span>Contactar</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-flex>
       </v-layout>
     </v-container>
@@ -47,6 +30,16 @@
 <script>
 
   export default {
-    
+    data() {
+      return {
+        team: [
+          { name: 'Sear', role: 'CEO' },
+          { name: 'Pito Perez', role: 'Diseñador Gráfico' },
+          { name: 'Minerva', role: 'Desarrollador Web' },
+          { name: 'Godofredo', role: 'Community Manager' },
+          { name: 'Jhora', role: 'Gerente Ventas' }
+        ]
+      }
+    },
   }
 </script>
