7dacb062b02f38a5c9614894d864e98b5a699c18
Searito <searclimaco@gmail.com>
2019-06-02 12:40:56 -0600

    Botones Para Ordenar Por Titulo del Proyecto y Por Creador

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,19 @@
     <h1 class="subheading blue-grey--text">Dashboard</h1>
 
     <v-container class="my-3">
+
+      <v-layout row class="mb-3">
+        <v-btn flat color="grey" @click="sortBy('title')">
+          <v-icon left small>folder</v-icon>
+          <span class="caption text-lowercase">Por Nombre</span>
+        </v-btn>
+        
+        <v-btn flat color="grey" @click="sortBy('person')">
+          <v-icon left small>person</v-icon>
+          <span class="caption text-lowercase">por creador</span>
+        </v-btn>
+      </v-layout>
+
       <v-card class="pa-3" v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
@@ -48,6 +61,13 @@
           { title: 'Diseñar Imagenes Adaptativas', person: 'Sear', due: '02/06/2019', status: 'Completo', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
           { title: 'Crear Foro', person: 'Pito Pérez', due: '03/06/2019', status: 'Retraso', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         ]
+      }
+    },
+
+    methods: {
+      sortBy(prop){
+        let self = this
+        self.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
       }
     },
   }
