bcaaabcba836a6e55ceb93a364258bde12a09b3c
Searito <searclimaco@gmail.com>
2019-06-09 12:39:25 -0600

    Formulario para creación de proyecto

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -6,6 +6,18 @@
            <v-card-title class="blue-grey lighten-4">
                <h2>Agregar Proyecto</h2>
            </v-card-title>
+
+           <v-card-text>
+               <v-form class="px-3">
+                   <VTextField v-model="title" label="Título" prepend-icon="folder"/>
+
+                   <VTextarea label="Información" v-model="content" prepend-icon="edit"/>
+
+                   <v-btn color="blue-grey lighten-4" class="mx-0 mt-3" @click="submit">
+                       Añadir Proyecto
+                    </v-btn>
+               </v-form>
+           </v-card-text>
        </VCard>
    </v-dialog>
 </template>
@@ -14,8 +26,17 @@
 export default {
     data(){
         return {
-            dialog: false
+            dialog: false,
+            title: '',
+            content: '',
         }
-    }
+    },
+
+    methods: {
+        submit(){
+            let self = this;
+            console.log(self.title, self.content);
+        }
+    },
 }
 </script>
